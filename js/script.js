10f93d71055e49faf6c992c369fb5dc76fbc54e8
Isabel Zhuang <izhu678@aucklanduni.ac.nz>
2016-09-09 17:41:52 +1200

    Removed code

--- js/script.js
+++ js/script.js
@@ -2,34 +2,27 @@ function doIt() {
     var hashtag = document.getElementById("hash").value;
 
     //console.log("do it is " + hashtag);
-    var output = $.ajax({
-        url: 'https://tagdef.p.mashape.com/one.' + hashtag + '.json',
-        type: 'GET',
-        data: {},
-        dataType: 'json',
-        success: function (data) {
-            //Change data.source to data.something , where something is whichever part of the object you want returned.
-            //To see the whole object you can output it to your browser console using:
-            //console.log(data);
-            //$("#output").before('<p>Defnition is: </p><br>');
-            document.getElementById("output").innerHTML = data.defs.def.text;
-        },
-        error: function (err) {
-            alert(err);
-        },
-        beforeSend: function (xhr) {
-            xhr.setRequestHeader("X-Mashape-Authorization", "CashpxF8LsmshHOQd1kcByYvzs7xp1zWCMijsnEjStLeAxWsvE"); // Enter here your Mashape key
-        }
-    });
+    if (hashtag == "") {
+        alert("Please enter a hashtag");
+    } else {
+        var output = $.ajax({
+            url: 'https://tagdef.p.mashape.com/one.' + hashtag + '.json',
+            type: 'GET',
+            data: {},
+            dataType: 'json',
+            success: function (data) {
+                //Change data.source to data.something , where something is whichever part of the object you want returned.
+                //To see the whole object you can output it to your browser console using:
+                //console.log(data);
+                //$("#output").before('<p>Defnition is: </p><br>');
+                document.getElementById("output").innerHTML = data.defs.def.text;
+            },
+            error: function (err) {
+                alert(err);
+            },
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader("X-Mashape-Authorization", "CashpxF8LsmshHOQd1kcByYvzs7xp1zWCMijsnEjStLeAxWsvE"); // Enter here your Mashape key
+            }
+        });
+    }
 }
-
-$(function () {
-    $("hash").keypress(function (e) {
-        if ((e.which && e.which == 13) || (e.keyCode && e.keyCode == 13)) {
-            $('button[type=submit] .default').click();
-            return false;
-        } else {
-            return true;
-        }
-    });
-});
